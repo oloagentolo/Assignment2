b3b94ade29b580de5fea61d405bc1be7ef2e9642
Ryan <oloagentolo@gmail.com>
2013-07-25 00:37:16 -0500

    Test for dependent destroy of work orders

--- factories.rb
+++ factories.rb
@@ -3,7 +3,15 @@ require File.expand_path('../worker', __FILE__)
 
 FactoryGirl.define do
   factory :worker do |worker|
-    worker.first_name  'FirstName'
-    worker.last_name   'LastName'
+    worker.first_name          'FirstName'
+    worker.last_name           'LastName'
+  end
+
+  factory :work_order do |work_order|
+    work_order.work_details        'The details of this work'
+    work_order.date_of_creation    DateTime.now
+    work_order.status              'In progress'
+    work_order.status_update_date  DateTime.now
+    work_order.association :worker
   end
 end
--- worker.rb
+++ worker.rb
@@ -2,5 +2,5 @@
 
 class Worker < ActiveRecord::Base
   validates_presence_of :last_name, :first_name
-  has_many :work_orders
+  has_many :work_orders, :dependent => :destroy
 end
